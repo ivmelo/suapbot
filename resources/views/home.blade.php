b9aba5065d817c18464144d948a4632fe3847484
vagrant <meloivanilson@gmail.com>
2016-09-18 20:37:53 +0000

    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,67 +3,83 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Users ({{ $users->count() }})</div>
+        <div class="col-md-10 col-md-offset-1">
 
-                <div class="panel-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Username</th>
-                                <th>Telegram ID</th>
-                                <th>Email</th>
-                                <th>SUAP ID</th>
-                                <th>Ntf</th>
-                                <th>Created</th>
-                                <th>Updated</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($users as $user)
-                            <tr>
-                                <td>{{ $user->id }}</td>
-                                <td>
-                                    {{ $user->first_name }} {{ $user->last_name }}
-                                    @if($user->is_admin)
-                                    <span class="label label-success">Admin</span>
-                                    @endif
-                                </td>
-                                <td><a href="{{ $user->username ? 'https://telegram.me/'.$user->username : '#' }}" target="_blank">{{ $user->username ? '@'.$user->username : '' }}</a></td>
-                                <td>{{ $user->telegram_id }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->suap_id }}</td>
-                                <td>
-                                    @if($user->notify)
-                                    <span class="label label-success"><span class="glyphicon glyphicon-ok"></span></span>
-                                    @else
-                                    <span class="label label-danger"><span class="glyphicon glyphicon-remove"></span></span>
-                                    @endif
-                                </td>
-                                <td>{{ $user->created_at->format('d/m/Y H:i:s') }}</td>
-                                <td>{{ $user->updated_at->format('d/m/Y H:i:s') }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+            <h2>Users ({{ $users->count() }})</h2>
+            <button type="button" class="btn btn-info pull-right" data-toggle="modal" data-target="#broadcastMessageModal">
+                Broadcast Message
+            </button>
+            <hr>
 
-                    <hr>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Username</th>
+                        <th>Telegram ID</th>
+                        <th>Email</th>
+                        <th>Ntf</th>
+                        <th>Created</th>
+                        <th>Updated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>
+                            {{ $user->first_name }} {{ $user->last_name }}
+                            @if($user->is_admin)
+                            <span class="label label-success">Admin</span>
+                            @endif
+                        </td>
+                        <td><a href="{{ $user->username ? 'https://telegram.me/'.$user->username : '#' }}" target="_blank">{{ $user->username ? '@'.$user->username : '' }}</a></td>
+                        <td>{{ $user->telegram_id }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            @if($user->notify)
+                            <span class="label label-success"><span class="glyphicon glyphicon-ok"></span></span>
+                            @else
+                            <span class="label label-danger"><span class="glyphicon glyphicon-remove"></span></span>
+                            @endif
+                        </td>
+                        <td>{{ $user->created_at->format('d/m/Y H:i:s') }}</td>
+                        <td>{{ $user->updated_at->format('d/m/Y H:i:s') }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
 
-                    <h4>Broadcast message</h4>
-                    <form action="{{ url('home') }}" method="post">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <textarea name="message" rows="8" placeholder="Message goes here..." class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" name="button" class="btn btn-info pull-right">Send!</button>
-                        </div>
-                    </form>
+            <hr>
+
+
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@section('modals')
+<div class="modal fade" id="broadcastMessageModal" tabindex="-1" role="dialog" aria-labelledby="broadcastMessageModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="{{ url('home') }}" method="post">
+                {{ csrf_field() }}
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="broadcastMessageModalLabel">Broadcast Message</h4>
                 </div>
-            </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <textarea name="message" rows="8" placeholder="Message to broadcast..." class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" name="button" class="btn btn-info pull-right">Send!</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
     <!-- <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css" media="screen"> -->
 
@@ -21,7 +20,7 @@
 
     <style>
         body {
-            font-family: 'Lato';
+            font-family: -apple-system, 'Lato', sans-serif;
         }
 
         .fa-btn {
@@ -83,6 +82,8 @@
     </nav>
 
     @yield('content')
+
+    @yield('modals')
 
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
