b654af6a8d1d2fb91ccfa9721c226f18d4e64d12
Ivan Melo <ivmelo@users.noreply.github.com>
2017-03-21 22:46:19 -0300

    Update CalendarioCommand.php

--- app/Telegram/Commands/CalendarioCommand.php
+++ app/Telegram/Commands/CalendarioCommand.php
@@ -30,7 +30,7 @@ class CalendarioCommand extends Command
 
         // And send message.
         $this->replyWithMessage([
-            'text' => "🗓 Calendário Acadêmico 2016: \n\nhttp://portal.ifrn.edu.br/campus/natalcentral/arquivos/calendario-escolar-2016-aprovado-pelo-conselho-escolar",
+            'text' => "🗓 Calendário Acadêmico 2017: \n\nhttp://portal.ifrn.edu.br/campus/natalcentral/arquivos/calendario-academico-de-referencia-2017",
             'parse_mode' => 'markdown'
         ]);
     }
